17a92efeb720891ed6aacaf6268d1665e210a04b
Cmerch91 <158065016+Cmerch91@users.noreply.github.com>
2024-03-13 14:48:38 +0000

    All salaries feature

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,16 +1,26 @@
 package com.napier.sem;
 
-import java.sql.*;
+import java.util.ArrayList;
+import java.sql.* ;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // Create new Application
         App a = new App();
 
         // Connect to database
         a.connect();
-        // Get Employees with title "Engineer"
-        a.getEmployeesByTitle("Engineer");
+
+        // Extract employee salary information
+        ArrayList<Employee> employees = a.getAllSalaries();
+
+        // Test the size of the returned data - should be 240124
+        System.out.println(employees.size());
+
+        // Print the salaries
+        a.printSalaries(employees);
+
         // Disconnect from database
         a.disconnect();
     }
@@ -65,33 +75,57 @@ public class App {
         }
     }
 
-    public void getEmployeesByTitle(String title) {
+    /**
+     * Gets all the current employees and salaries.
+     *
+     * @return A list of all employees and salaries, or null if there is an error.
+     */
+    public ArrayList<Employee> getAllSalaries() {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary " +
-                            "FROM employees " +
-                            "JOIN titles ON employees.emp_no = titles.emp_no " +
-                            "JOIN salaries ON employees.emp_no = salaries.emp_no " +
-                            "WHERE titles.to_date = '9999-01-01' " +
-                            "AND titles.title = '" + title + "' " +
-                            "AND salaries.to_date = '9999-01-01' " +
-                            "ORDER BY employees.emp_no ASC";
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
+                            + "FROM employees, salaries "
+                            + "WHERE employees.emp_no = salaries.emp_no AND salaries.to_date = '9999-01-01' "
+                            + "ORDER BY employees.emp_no ASC";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
-            // Display results
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<Employee>();
             while (rset.next()) {
-                int emp_no = rset.getInt("emp_no");
-                String first_name = rset.getString("first_name");
-                String last_name = rset.getString("last_name");
-                int salary = rset.getInt("salary");
-                System.out.println(emp_no + " " + first_name + " " + last_name + " Salary: " + salary);
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
             }
+            return employees;
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get employees details");
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
+    /**
+     * Prints a list of employees.
+     *
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
         }
     }
 }
