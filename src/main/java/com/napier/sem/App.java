ecf94d634e935f114d3733f172cf1f87f0248162
Cmerc91 <cmerchant622@gmail.com>
2024-04-16 23:46:21 +0100

    App updated - Arraylist added, new feature branch.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,22 +1,23 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Create new Application
         App a = new App();
-
         // Connect to database
         a.connect();
-        // Get Employee
-        Employee emp = a.getEmployee(255530);
-        // Display results
-        a.displayEmployee(emp);
-
+        // Get Employees with the title "Engineer"
+        List<Employee> engineers = a.getEmployee("Engineer");
+        // Display results for each engineer
+        for (Employee emp : engineers) {
+            a.displayEmployee(emp);
+        }
         // Disconnect from database
         a.disconnect();
     }
@@ -70,51 +71,48 @@ public class App {
             }
         }
     }
-    public Employee getEmployee(int ID)
-    {
-        try
-        {
+    public List<Employee> getEmployee(String title) {
+        List<Employee> employees = new ArrayList<>();
+        try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary " +
+                            "FROM employees " +
+                            "JOIN salaries ON employees.emp_no = salaries.emp_no AND salaries.to_date = '9999-01-01' " +
+                            "JOIN titles ON employees.emp_no = titles.emp_no AND titles.to_date = '9999-01-01' " +
+                            "WHERE titles.title = '" + title + "' " +
+                            "ORDER BY employees.emp_no ASC";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
-            // Check one is returned
-            if (rset.next())
-            {
+            // Loop through the result set
+            while (rset.next()) {
                 Employee emp = new Employee();
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
-                return emp;
+                emp.salary = (int) rset.getDouble("salary");
+                employees.add(emp);
             }
-            else
-                return null;
+        } catch (SQLException e) {
+            System.out.println("Failed to get employees by title: " + e.getMessage());
         }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
-            return null;
-        }
+        return employees;
     }
+
     public void displayEmployee(Employee emp)
     {
         if (emp != null)
         {
             System.out.println(
-                    emp.emp_no + " "
-                            + emp.first_name + " "
-                            + emp.last_name + "\n"
-                            + emp.title + "\n"
-                            + "Salary:" + emp.salary + "\n"
-                            + emp.dept_name + "\n"
-                            + "Manager: " + emp.manager + "\n");
+                    emp.emp_no + " " +
+                            emp.first_name + " " +
+                            emp.last_name + " " +
+                            emp.title + " " +
+                            "Salary:" + emp.salary + " " +
+                            emp.dept_name + " " +
+                            "Manager: " + emp.manager + " ");
         }
     }
 }
