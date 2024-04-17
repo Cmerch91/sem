c85f3c67ff7830570f40815b3062eb22ca1bfdb3
Cmerc91 <cmerchant622@gmail.com>
2024-04-17 20:36:27 +0100

    New Main

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -151,20 +151,23 @@ public class App {
      *
      * @param employees An ArrayList of Employee objects.
      */
-    public void printSalaries(ArrayList<Employee> employees) {
-        if (employees == null) {
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Check employees is not null
+        if (employees == null)
+        {
             System.out.println("No employees");
             return;
         }
-
-        System.out.println(String.format("%-10s %15s %20s %8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
         for (Employee emp : employees)
-
         {
             if (emp == null)
                 continue;
             String emp_string =
-                    String.format("%10s %15s %20s &8s",
+                    String.format("%-10s %-15s %-20s %-8s",
                             emp.emp_no, emp.first_name, emp.last_name, emp.salary);
             System.out.println(emp_string);
         }
